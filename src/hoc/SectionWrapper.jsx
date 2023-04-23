48fc57479d740c002848f3763c96b9bc4ed21e4b
George <xsova113@gmail.com>
2023-04-23 14:42:27 -0700

    minor update

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -11,9 +11,8 @@ const SectionWrapper = (Component, idName) =>
                 whileInView="show"
                 viewport={{ once: true, amount: 0.25 }}
                 className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
-                id={idName}
             >
-                <span className="hash-span">
+                <span className="hash-span" id={idName}>
                     &nbsp;
                 </span>
                 <Component />
